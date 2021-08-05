7e104b8538a514f0d49d53d8d9957b9456abfc8b
YoannDrouet <yoanndrouet@hotmail.fr>
2021-08-05 15:32:01 +0200

    Ajout formulaire

---
+++ src/Form/WishType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Wish;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class WishType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('author')
+            ->add('dateCreated', DateType::class,
+                ['html5'=>true, 'widget'=>'single_text'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Wish::class,
+        ]);
+    }
+}
