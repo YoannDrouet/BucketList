2ff64d8d23ac8782f05303b0583fd17750a77e84
YoannDrouet <yoanndrouet@hotmail.fr>
2021-08-09 10:58:53 +0200

    Ajout modification

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -42,14 +42,17 @@ class WishController extends AbstractController
     public function update(Wish $wish, EntityManagerInterface $em, Request $request):Response {
         $formWish = $this->createForm(WishType::class, $wish);
         $formWish->handleRequest($request);
+        $message = [];
 
         if ($formWish->isSubmitted() && $formWish->isValid()){
-
+            $message = ["indice"=>"success", "message"=>"Modification effectuée"];
             $em->flush();
-            return $this->redirectToRoute('home');
+            return $this->redirectToRoute('home', $message);
+        } elseif ($formWish->isSubmitted() && !$formWish->isValid()){
+            $message = ["indice"=>"danger", "message"=>"Modification impossible"];
         }
 
-        return $this->render('main/addIdea.html.twig', ['wish'=>$wish, "formWish"=>$formWish->createView()]);
+        return $this->render('main/addIdea.html.twig', ['wish'=>$wish, 'flash'=>$message, "formWish"=>$formWish->createView()]);
     }
 
     /**
